429b008b948e85a9c2a475395b8b143b89f12f24
DareenAlharbi <Dareenbkheet@gmail.com>
2024-04-01 10:18:21 +0300

    de commit

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,7 +16,7 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
       System.out.print("hi");
-            
+        System.out.println("Hello im dedi");     
     }
     
 }
